bfbcbe0973d3dada32c020db534816b3f5dca580
Greg App <greg358@mail.ru>
2022-10-08 02:25:36 +0300

    feat: add adaptive interactive burger menu to both pages

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -39,3 +39,27 @@ function ifValidBtn (e) {
     inputBtn.addEventListener ('click',ifValidMain);
 document.body.addEventListener ('focusin',ifValidMain);
 document.body.addEventListener ('focusout',ifValidMain); */
+
+
+const burgerMenu = document.querySelector (".burger-menu");
+const siteCover = document.querySelector(".cover");
+const burgerCross = document.querySelector(".btn-cross");
+const burgerBtn = document.querySelector(".burger-btn");
+burgerBtn.addEventListener('click',showMenu);
+siteCover.addEventListener('click',hideMenu);
+burgerCross.addEventListener('click',hideMenu);
+function showMenu () {
+    if (!burgerMenu.classList.contains('burger-active')) {
+        burgerMenu.classList.add('burger-active');
+        siteCover.classList.add('enable');
+    } else {
+        burgerMenu.classList.remove('burger-active');
+        siteCover.classList.remove('enable');
+    }
+}
+function hideMenu () {
+    if (burgerMenu.classList.contains('burger-active')) {
+        burgerMenu.classList.remove('burger-active');
+        siteCover.classList.remove('enable');
+    }
+}
