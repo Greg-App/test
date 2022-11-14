6a884fabcf4b66070f78daed14673f779c2e2bc4
Greg App <greg358@mail.ru>
2022-11-14 04:19:24 +0300

    feat: create gaming mechanism (select and mark bird-list-item, then show bird-info)

--- songbird/pages/quiz/script.js
+++ songbird/pages/quiz/script.js
@@ -1,4 +1,5 @@
 import birdsData from '../../app/js/birds-list.js';
+import birdCardHTML from '../../app/js/bird-card.js';
 //import * as playerModule from '../../app/js/player';
 
 function getRandomNum(min, max) {
@@ -9,22 +10,33 @@ console.log(birdsData);
 const state = {
   stage: 0,
   score: 0,
+  defBirdInfo: 'Послушайте плеер.<br>Выберите птицу из списка',
+  curBirdInfo: null,
+  tryCount: 0
 };
+
+function setDefBirdInfo() {
+  const birdInfoBlock = document.querySelector('.bird-card-info');
+  birdInfoBlock.innerHTML = state.defBirdInfo;
+}
+setDefBirdInfo();
 let curBird = birdsData[state.stage][getRandomNum(0, birdsData.length - 1)];
 console.log('curBird: ', curBird);
-
+state.curBirdInfo = curBird;
+console.log(state);
 /*----create/update bird list, current stage and score START--------*/
 function updScore() {
-  const score=document.querySelector('.dash-score');
-  score.textContent=state.score;
-  }
+  const score = document.querySelector('.dash-score');
+  score.textContent = state.score;
+}
 
 const birdList = document.querySelector('.bird-list');
-  birdList.addEventListener('click', selectBird);
+birdList.addEventListener('click', selectBird);
+
 function createBirdList() {
-    if (birdList.children) {
-      birdList.replaceChildren();
-    }
+  if (birdList.children) {
+    birdList.replaceChildren();
+  }
   birdsData[state.stage].forEach((el) => {
     const birdItem = document.createElement('li');
     birdItem.classList.add('bird-list__list-item');
@@ -40,16 +52,51 @@ function createBirdList() {
 }
 
 function updateCurrStage() {
-const oldstageAct = document.querySelector('.quiz-nav__list-item_active');
-if(oldstageAct) {oldstageAct.classList.remove('quiz-nav__list-item_active');}
-const stageList = document.querySelectorAll('.quiz-nav__list-item');
-stageList[state.stage].classList.toggle('quiz-nav__list-item_active');
+  const oldstageAct = document.querySelector('.quiz-nav__list-item_active');
+  if (oldstageAct) {
+    oldstageAct.classList.remove('quiz-nav__list-item_active');
+  }
+  const stageList = document.querySelectorAll('.quiz-nav__list-item');
+  stageList[state.stage].classList.toggle('quiz-nav__list-item_active');
 }
 createBirdList();
 updateCurrStage();
 updScore();
 /*----create/update bird list, current stage and score END--------*/
 
+/*----create/update bird info block     START--------*/
+
 function selectBird(e) {
-  console.log('hey');
+  state.tryCount += 1;
+  if (e.target.closest('.bird-list__list-item')) {
+    console.log('hey');
+    const selBirdName = e.target.closest('.bird-list__list-item');
+    selBirdName.classList.add('bird-list__list-item_checked');
+    if (selBirdName.children[1].textContent.toLowerCase().trim() == state.curBirdInfo.name.toLowerCase()) {
+      /*win*/
+      selBirdName.children[0].style.background = '#008000';
+      const nextBtn =document.querySelector('.next-level-btn');
+      nextBtn.classList.add('next-level-btn_active');
+    } else {
+      selBirdName.children[0].style.background = '#c7300b';
+    }
+    for (let key of birdsData[state.stage]) {
+      if (key.name.toLowerCase() == selBirdName.children[1].textContent.toLowerCase().trim()) {
+        const birdInfoBlock = document.querySelector('.bird-card-info');
+        birdInfoBlock.innerHTML = birdCardHTML;
+        const birdCardImg = birdInfoBlock.querySelector('.bird-card__img');
+        birdCardImg.src = key.image;
+        birdCardImg.alt = `bird-img ${key.name}`;
+        const birdCardName = birdInfoBlock.querySelector('.bird-card__name');
+        birdCardName.textContent = key.name;
+        const birdCardSpecies = birdInfoBlock.querySelector('.bird-card__gen');
+        birdCardSpecies.textContent = key.species;
+        const birdCardDescr = birdInfoBlock.querySelector('.bird-card__description');
+        birdCardDescr.textContent = key.description;
+      }
+
+    }
+
+
   }
+}
