fa2f1cec1039bcea75ac57dd4425c1d1e9652494
Greg App <greg358@mail.ru>
2022-11-15 07:42:56 +0300

    fix: fix player issues(now dont have drapndrop and progress value style)

--- songbird/pages/quiz/script.js
+++ songbird/pages/quiz/script.js
@@ -1,6 +1,8 @@
 import birdsData from '../../app/js/birds-list.js';
 import birdCardHTML from '../../app/js/bird-card.js';
-import {birdPlayerHTML} from '../../app/js/bird-card.js';
+import {
+  birdPlayerHTML
+} from '../../app/js/bird-card.js';
 //import * as playerModule from '../../app/js/player';
 
 function getRandomNum(min, max) {
@@ -28,7 +30,7 @@ console.log('curBird: ', curBird);
 state.curBirdInfo = curBird;
 console.log(state);
 /*-create preview birdCard---------*/
-createBirdCard('bird-card-play',curBird,birdCardHTML);
+createBirdCard('bird-card-play', curBird, birdCardHTML);
 
 /*----create/update bird list, current stage and score START--------*/
 function updateScore() {
@@ -81,13 +83,13 @@ function selectBird(e) {
 
     if (selBirdName.children[1].textContent.toLowerCase().trim() == state.curBirdInfo.name.toLowerCase()) {
       /*win*/
-      const clicksound =document.querySelector('.win');
+      const clicksound = document.querySelector('.win');
       clicksound.play();
       const allAudioBtn = document.querySelectorAll('.bird-card .play');
-      allAudioBtn.forEach((el)=>{
-        if(el.classList.contains('pause')) {
+      allAudioBtn.forEach((el) => {
+        if (el.classList.contains('pause')) {
           el.click();
-        } 
+        }
       });
       /* const allAudio = document.querySelectorAll('.bird-card .audio');
       const allPlayerIcon = document.querySelectorAll('.player-icon');
@@ -99,9 +101,9 @@ function selectBird(e) {
       }); */
       if (!selBirdName.classList.contains('bird-list__list-item_checked')) {
         state.score = state.score + birdsData[state.stage].length - state.tryCount;
-        addBirdCardInfo('bird-card-play',curBird);
-        if(state.stage===birdsData.length-1) {
-          localStorage.setItem('songbird-score',`${state.score}`);
+        addBirdCardInfo('bird-card-play', curBird);
+        if (state.stage === birdsData.length - 1) {
+          localStorage.setItem('songbird-score', `${state.score}`);
         }
       }
       updateScore();
@@ -112,7 +114,7 @@ function selectBird(e) {
       nextBtn.disabled = false;
 
     } else {
-      const clicksound =document.querySelector('.loose');
+      const clicksound = document.querySelector('.loose');
       clicksound.play();
       selBirdName.children[0].style.background = '#c7300b';
       selBirdName.classList.add('bird-list__list-item_checked');
@@ -121,121 +123,133 @@ function selectBird(e) {
     if (!birdInfoSelName || birdInfoSelName.textContent.toLowerCase().trim() !== selBirdName.children[1].textContent.toLowerCase().trim()) {
       for (let key of birdsData[state.stage]) {
         if (key.name.toLowerCase() == selBirdName.children[1].textContent.toLowerCase().trim()) {
-          createFullBirdCard('bird-card-info',key,birdCardHTML);
+          createFullBirdCard('bird-card-info', key, birdCardHTML);
         }
       }
     }
   }
 }
-function createPlayer(inpClass,birdObj) {
-  const birdInfoBlock = document.querySelector(`.${inpClass}`);
-  const birdCardPlayer =birdInfoBlock.querySelector('.bird-card__player');
-  birdCardPlayer.innerHTML=birdPlayerHTML;
-  const audioTrack =birdCardPlayer.querySelector('.audio');
-  audioTrack.src=birdObj.audio;
-  
-  let isPlay = false;
-let trackNum = 0;
-const playBtn = birdCardPlayer.querySelector('.play');
-const audio = birdCardPlayer.querySelector('.audio');
-playBtn.addEventListener('click', togglePlay);
-let curPlayTime = 0;
-function playAudio() {
-    audio.play();
-    audio.currentTime = curPlayTime;
-    isPlay = true;
-}
 
-function pauseAudio() {
+function createPlayer(inpClass, birdObj) {
+  const birdInfoBlock = document.querySelector(`.${inpClass}`);
+  const birdCardPlayer = birdInfoBlock.querySelector('.bird-card__player');
+  birdCardPlayer.innerHTML = birdPlayerHTML;
+  const audioTrack = birdCardPlayer.querySelector('.audio');
+  audioTrack.src = birdObj.audio;
+
+  let isPlay = false;
+  let trackNum = 0;
+  const playBtn = birdCardPlayer.querySelector('.play');
+  const audio = birdCardPlayer.querySelector('.audio');
+  playBtn.addEventListener('click', togglePlay);
+  audio.addEventListener('ended', ()=>{
+    playBtn.classList.toggle('pause');
+    isPlay=false;
+    curPlayTime = 0;
+    console.log('END');
+    console.log(isPlay);
+  });
+
+  let curPlayTime = 0;
+
+  function playAudio() {
+    audio.currentTime = curPlayTime;
+    audio.play();
+    isPlay = true;
+  }
+
+  function pauseAudio() {
     curPlayTime = audio.currentTime;
     audio.pause();
     audio.currentTime = curPlayTime;
     isPlay = false;
-}
+  }
 
-function togglePlay() {
+  function togglePlay() {
     if (isPlay === false) {
-        playAudio();
-        playBtn.classList.toggle('pause');
+      playAudio();
+      playBtn.classList.toggle('pause');
     } else {
-        pauseAudio();
-        playBtn.classList.toggle('pause');
+      pauseAudio();
+      playBtn.classList.toggle('pause');
     }
-}
+  }
 
-/*-------Audio Player Enhanced (custom)-START---------*/
 
-function formatTime(seconds) {
+  /*-------Audio Player Enhanced (custom)-START---------*/
+
+  function formatTime(seconds) {
     let min = Math.floor((seconds / 60));
     let sec = Math.floor(seconds - (min * 60));
     if (sec < 10) {
-        sec = `0${sec}`;
+      sec = `0${sec}`;
     }
     return `${min}:${sec}`;
-}
+  }
 
-function updateCurTime() {
+  function updateCurTime() {
     const curTime = birdCardPlayer.querySelector('.cur-time');
     const durTime = birdCardPlayer.querySelector('.total-dur');
 
     if (isNaN(audio.duration)) {
-        curTime.textContent = '00:00';
-        durTime.innerHTML = '00:00';
+      curTime.textContent = '00:00';
+      durTime.innerHTML = '00:00';
     } else {
-        curTime.textContent = formatTime(Math.floor(audio.currentTime));
-        durTime.textContent = formatTime(Math.floor(audio.duration));
+      curTime.textContent = formatTime(Math.floor(audio.currentTime));
+      durTime.textContent = formatTime(Math.floor(audio.duration));
     }
-}
-updateCurTime();
-const playBar = birdCardPlayer.querySelector('#playBar');
+  }
+  updateCurTime();
+  const playBar = birdCardPlayer.querySelector('#playBar');
 
-playBar.addEventListener("click", e => {
+  playBar.addEventListener("mousedown", e => {
     const playBarWidth = window.getComputedStyle(playBar).width;
     const curTime = e.offsetX / parseInt(playBarWidth) * audio.duration;
     audio.currentTime = curTime;
     curPlayTime = curTime;
-}, false);
-
-function updatePlayBar() {
-    playBar.max = audio.duration;
+  }, false);
+  //setInterval(()=>{}, 800);
+  audio.addEventListener('loadedmetadata',()=>{playBar.max = audio.duration;});
+  function updatePlayBar() {
     playBar.value = audio.currentTime;
     updateCurTime();
-}
-setInterval(updatePlayBar, 500);
+  }
+  setInterval(updatePlayBar, 10);
 
-const volumeBtn = birdCardPlayer.querySelector('.volume-icon img');
-volumeBtn.addEventListener('click', mute);
+  const volumeBtn = birdCardPlayer.querySelector('.volume-icon img');
+  volumeBtn.addEventListener('click', mute);
 
-function mute() {
+  function mute() {
     if (audio.muted === false) {
-        audio.muted = true;
-        volumeBtn.src = '../../assets/icons/volume-off-svgrepo-com.svg';
+      audio.muted = true;
+      volumeBtn.src = '../../assets/icons/volume-off-svgrepo-com.svg';
     } else {
-        audio.muted = false;
-        volumeBtn.src = '../../assets/icons/volume-low-svgrepo-com.svg';
+      audio.muted = false;
+      volumeBtn.src = '../../assets/icons/volume-low-svgrepo-com.svg';
     }
-}
+  }
 
-const volBar = birdCardPlayer.querySelector('.volume-bar');
-volBar.addEventListener('click', changeVolume);
+  const volBar = birdCardPlayer.querySelector('.volume-bar');
+  volBar.addEventListener('mousedown', changeVolume);
 
-function changeVolume(e) {
+  function changeVolume(e) {
     const volBarWidth = window.getComputedStyle(volBar).width;
     const volCurrent = birdCardPlayer.querySelector('.volume-current');
     audio.volume = (e.offsetX / parseInt(volBarWidth));
     volCurrent.style.width = audio.volume * 100 + '%';
+  }
+
+  /*-------Audio Player Enhanced (custom)-END--------------------------------*/
 }
 
-/*-------Audio Player Enhanced (custom)-END---------*/
-}
-function addBirdCardInfo(inpClass,birdObj)  {
+function addBirdCardInfo(inpClass, birdObj) {
   const birdInfoBlock = document.querySelector(`.${inpClass}`);
   const birdCardImg = birdInfoBlock.querySelector('.bird-card__img');
-  const img=new Image();
-  img.src=birdObj.image;
+  const img = new Image();
+  img.src = birdObj.image;
   img.addEventListener('load', () => {
     birdCardImg.src = birdObj.image;
-});
+  });
   birdCardImg.alt = `bird-img ${birdObj.name}`;
   const birdCardName = birdInfoBlock.querySelector('.bird-card__name');
   birdCardName.textContent = birdObj.name;
@@ -244,14 +258,16 @@ function addBirdCardInfo(inpClass,birdObj)  {
   const birdCardDescr = birdInfoBlock.querySelector('.bird-card__description');
   birdCardDescr.textContent = birdObj.description;
 }
-function createBirdCard(inpClass,birdObj,htmlStr) {
+
+function createBirdCard(inpClass, birdObj, htmlStr) {
   const birdInfoBlock = document.querySelector(`.${inpClass}`);
   birdInfoBlock.innerHTML = htmlStr;
-  createPlayer(inpClass,birdObj);
+  createPlayer(inpClass, birdObj);
 }
-function createFullBirdCard(inpClass,birdObj,htmlStr) {
-  createBirdCard(inpClass,birdObj,htmlStr);
-  addBirdCardInfo(inpClass,birdObj);
+
+function createFullBirdCard(inpClass, birdObj, htmlStr) {
+  createBirdCard(inpClass, birdObj, htmlStr);
+  addBirdCardInfo(inpClass, birdObj);
 }
 
 
@@ -270,7 +286,7 @@ function moveToNextLevel(e) {
   curBird = birdsData[state.stage][getRandomNum(0, birdsData.length - 1)];
   console.log('curBird: ', curBird);
   state.curBirdInfo = curBird;
-  createBirdCard('bird-card-play',curBird,birdCardHTML);
+  createBirdCard('bird-card-play', curBird, birdCardHTML);
 }
 
 
