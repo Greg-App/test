ac10a748c58cda22fb2b49056cf252bcdbdc3e73
Greg App <greg358@mail.ru>
2022-12-12 14:14:16 +0300

    migrate app.js, index.js to TS

---
+++ news-JS/src/components/app/app.ts
@@ -0,0 +1,22 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import querySelectSave from '../controller/helper';
+import {IsrcObj,InewsObj,Inews,Isources} from '../controller/helper';
+
+class App {
+  controller: AppController;
+  view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+      
+      querySelectSave(document,HTMLElement,'.sources')
+            .addEventListener('click', (e) => this.controller.getNews(e, (data: Inews) => this.view.drawNews(data)));
+        this.controller.getSources((data:Isources) => this.view.drawSources(data));
+    }
+}
+
+export default App;
